1b2d656a07302d7853ea05f403f9e02e76577308
Shawn <shawn.chou@nogle.com>
2020-01-20 17:13:17 +0800

    1. init project

---
+++ src/main/java/name/qd/tech_linebot_spring/Application.java
@@ -0,0 +1,11 @@
+package name.qd.tech_linebot_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] s) {
+		SpringApplication.run(Application.class, s);
+	}
+}
