3953cea521a3453289979eaf7d837c9d3d83f01b
Shawn <shawn.chou@nogle.com>
2020-01-21 19:38:41 +0800

    1. test

--- src/main/java/name/qd/linebot/spring/handler/ChatMessageHandler.java
+++ src/main/java/name/qd/linebot/spring/handler/ChatMessageHandler.java
@@ -2,7 +2,10 @@ package name.qd.linebot.spring.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -14,16 +17,21 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @LineMessageHandler
 public class ChatMessageHandler {
 	private final Logger log = LoggerFactory.getLogger(ChatMessageHandler.class);
+
+	@Autowired
+    private LineMessagingClient lineMessagingClient;
 	
 	@EventMapping
-    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        log.info("handleTextMessageEvent: " + event);
-        final String originalMessageText = event.getMessage().getText();
-        return new TextMessage(originalMessageText);
-    }
+	public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		lineMessagingClient.replyMessage(new ReplyMessage(event.getReplyToken(), new TextMessage("HIHIHIHIHIHI")));
+		log.info("handleTextMessageEvent: " + event);
+		final String originalMessageText = event.getMessage().getText();
+		return new TextMessage(originalMessageText);
+	}
 
-    @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-    	log.info("handleDefaultMessageEvent: " + event);
-    }
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		System.out.println("handleDefaultMessageEvent");
+		log.info("handleDefaultMessageEvent: " + event);
+	}
 }
