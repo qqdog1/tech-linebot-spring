d722132373121fc41f888190e503105171af7879
Shawn <shawn.chou@nogle.com>
2020-01-21 17:40:38 +0800

    1. add spring boot plugin 2. add test rest api

--- src/main/java/name/qd/linebot/spring/Application.java
+++ src/main/java/name/qd/linebot/spring/Application.java
@@ -2,10 +2,18 @@ package name.qd.linebot.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] s) {
 		SpringApplication.run(Application.class, s);
 	}
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String test() {
+      return "Hi!";
+    }
 }
