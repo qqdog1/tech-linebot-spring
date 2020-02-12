8634d75002a78531a762ee501a17b8f2de9fa148
shawn <shawn.chou@nogle.com>
2020-02-12 22:51:59 +0800

    1. create DataInputController

---
+++ src/main/java/name/qd/linebot/spring/controller/DataInputController.java
@@ -0,0 +1,10 @@
+package name.qd.linebot.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/data")
+public class DataInputController {
+
+}
